2eeb1a1868e250f6e43764b4df1d5270078e9e72
SungJieundev <sungjieun.dev@gmail.com>
2022-09-07 12:57:51 +0900

    2022-09-07 PlayerMove

---
+++ Assets/01_Scripts/PlayerMove.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] 
+    private float _speed = 5f;
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move(){
+        float hori = Input.GetAxisRaw("Horizontal");
+        float verti = Input.GetAxisRaw("Vertical");
+
+        transform.Translate(new Vector3(hori, verti, 0).normalized * _speed * Time.deltaTime);
+    }
+}
