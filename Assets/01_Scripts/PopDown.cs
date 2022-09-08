cfaeb9d5eff8bb1c69a6e2252f907a767c88222a
SungJieundev <sungjieun.dev@gmail.com>
2022-09-08 11:44:40 +0900

    2022-09-08 PopDown

--- Assets/01_Scripts/PopDown.cs
+++ Assets/01_Scripts/PopDown.cs
@@ -3,18 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
-public class PopUp : MonoBehaviour
-{   
+public class PopDown : MonoBehaviour
+{
+     [SerializeField] GameObject panel;
 
-    [SerializeField] GameObject panel;
-
-    public void PopUpPanel()
-    {
-        panel.SetActive(true);
+     void PopDownPanel(){
 
         Sequence seq = DOTween.Sequence();
 
         seq.Append(panel.transform.DOScale(new Vector3(1.05f, 1.05f), 0.4f));
-        seq.Append(panel.transform.DOScale(new Vector3(1f, 1f), 0.2f));
-    }
+        seq.Append(panel.transform.DOScale(new Vector3(0, 0), 0.2f));
+
+        panel.SetActive(false);
+     }
 }
