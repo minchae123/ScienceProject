4acac58d36b08e2ca8246c2bd5dc4673c636194e
SungJieundev <sungjieun.dev@gmail.com>
2022-09-08 11:34:52 +0900

    2022-09-08 PopUp

---
+++ Assets/01_Scripts/PopUp.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.tweening;
+
+public class PopUp : MonoBehaviour
+{   
+
+    [SerializeField] GameObject panel;
+
+    public void PopUpPanel()
+    {
+        panel.SetActive(true);
+
+        Sequence seq = DOTween.Sequence();
+
+        seq.Append(panel.transform.DOScale(new Vector3(1.05f, 1.05f), 0.4f));
+        seq.Append(panel.transform.DOScale(new Vector3(1f, 1f), 0.2f));
+    }
+}
