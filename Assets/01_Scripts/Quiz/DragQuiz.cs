5aadda8b1b736ee6c463efa2151a17586fdc5644
minchae123 <08minchae17@gmail.com>
2022-09-15 00:09:01 +0900

    퀴즈 2

---
+++ Assets/01_Scripts/Quiz/DragQuiz.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragQuiz : MonoBehaviour, IPointerDownHandler, IDragHandler
+{
+    public void OnDrag(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+
+    }
+
+}
---
+++ Assets/01_Scripts/Test/ColliderTest.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderTest : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log(22);
+        }
+    }
+
+}
