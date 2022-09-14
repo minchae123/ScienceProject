e3f44ad8a39ddfeee90406560076abc7ffae0bd8
minchae123 <08minchae17@gmail.com>
2022-09-14 16:40:08 +0900

    0914

---
+++ Assets/01_Scripts/Quiz/ChooseSol.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChooseSol : MonoBehaviour
+{
+    public void Collect()
+    {
+        Debug.Log("Á¤´ä");
+    }
+
+    public void False()
+    {
+        Debug.Log("¶¯¶¯");
+
+
+    }
+}
